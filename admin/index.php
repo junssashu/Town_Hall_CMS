abd3cf768a1cb566fe242630c06526291a5606db
Brel Mbe <brelmbe@gmail.com>
2022-06-01 12:03:19 +0100

    pk

--- admin/index.php
+++ admin/index.php
@@ -1,44 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=Acceuil Marie, initial-scale=1.0">
-    <title>Administrateur</title>
-    <link rel="stylesheet" href="assets/styles/index.css" ></link>
-</head>
-<body>
+<link rel="stylesheet" href="../assets/styles/index.css" >
 
-    <div class="header">
-        <div class="title">
-            CMS pour la réalisation du site web d'une mairie<br><br>
-        </div>
+
+<div class="header">
+    <div class="title">
+        CMS pour la réalisation du site web d'une mairie<br><br>
     </div>
-    <div class="main-content">
-        <div class="description">
-            Ceci est un cms<br>
-            Plus précisément, cette application web (cms) vous permettre ade réaliser aisément votre site web présentant votre mairie.<br>
-            Aucune connaissance en programmation web n'est requise, vous n'avez qu'à utiliser ce qui vous est présenté.
-            <br><br>Vous etes actuellement connectés en mode admin.<br><br><br>
-        </div>
-        <div class="instruction">
-            Veuillez choisir une rubrique pour effectuer des actions:
-        </div>
-        <div class="options">
-            <a href="pages/Presentation_mairie/menu.php" class="option">Présentation de la mairie</a>
-            <a href="pages/Activite_mairie/activites.php" class="option">Activités</a>
-            <a href="pages/Annonces_mairie/annonces.php" class="option">Annonces</a>
-            <a href="pages/Espace_pub_mairie/pubs.php" class="option">Espace publicitaire</a>
-            <a class="option">Lieux touristiques</a>
-            <a class="option">Projets</a>
-        </div>
-
-        
-        <div class="footer">
-            <br><br><br> Par les étudiants de la faculté des sciences de l'université de Yaoundé I.<br>
-            Pour le Travail pratique de l'unité d'enseignement INF2064, programmation web.
-        </div>
+</div>
+<div class="main-content">
+    <div class="description">
+        Ceci est un cms<br>
+        Plus précisément, cette application web (cms) vous permettra de réaliser aisément votre site web présentant votre mairie.<br>
+        Aucune connaissance en programmation web n'est requise, vous n'avez qu'à utiliser ce qui vous est présenté.
+        <br><br>Vous etes actuellement connectés en mode admin.<br><br><br>
+    </div>
+    <div class="instruction">
+        Veuillez choisir une rubrique pour effectuer des actions:
+    </div>
+    <div class="options">
+        <a href="pages/Presentation_mairie/menu.php" class="option">Présentation de la mairie</a>
+        <a href="pages/Activite_mairie/activites.php" class="option">Activités</a>
+        <a href="pages/Annonces_mairie/annonces.php" class="option">Annonces</a>
+        <a href="pages/Espace_pub_mairie/pubs.php" class="option">Espace publicitaire</a>
+        <a href="pages/Lieux_touristiques_mairie/sites_touristiques.php" class="option">Lieux touristiques</a>
+        <a href="pages/Projet_marie/projets_courants.php" class="option">Projets</a>
     </div>
 
-</body>
-</html>
+    
+</div>
+<div class="footer">
+    <br><br><br> Par les étudiants de la faculté des sciences de l'université de Yaoundé I.<br>
+    Pour le Travail pratique de l'unité d'enseignement INF2064, programmation web.
+</div>
