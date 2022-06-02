b492ee663e8bb867c8e9c064dc387df9073ed4ba
Yannis117 <nenzekoyannis@outlook.com>
2022-06-02 18:12:27 +0100

    debut du style de index.php du dossier admin

--- admin/pages/Activite_mairie/activites.php
+++ admin/pages/Activite_mairie/activites.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../../assets/styles/index.css" >
+<link rel="stylesheet" href="../../assets/styles/index.css">
 
 
 <div class="header">
@@ -13,19 +13,21 @@
     <div class="instruction">
         Gérer les activités de la mairie.
     </div>
-    
+
     <div class="cadre">
-       <a href="creer_activite.php" class="">Ajouter une activité</a>
+        <a href="creer_activite.php" class="">Ajouter une activité</a>
         <?php
-            /*Affichage des activités (dans un tableau).
+        /*Affichage des activités (dans un tableau).
             Le dernier champ du tableau porte le nom action et renverra vers la suppression ou la modification d'une activité.*/
-        
+
         ?>
 
     </div>
-    
 </div>
-<div class="footer">
-    <br><br><br> Par les étudiants de la faculté des sciences de l'université de Yaoundé I.<br>
-    Pour le Travail pratique de l'unité d'enseignement INF2064, programmation web.
-</div>
+
+<footer>
+    <div class="footer">
+        <br><br><br> Par les étudiants de la faculté des sciences de l'université de Yaoundé I.<br>
+        Pour le Travail pratique de l'unité d'enseignement INF2064, programmation web.
+    </div>
+</footer>
