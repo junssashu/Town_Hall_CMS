2f03b140846ef45fd61fede64b3da8b60e828cf8
Yannis117 <nenzekoyannis@outlook.com>
2022-06-03 13:36:10 +0100

    pseudo finalisation numero 2 du style de index.php du dossier admin

--- admin/index.php
+++ admin/index.php
@@ -21,25 +21,33 @@
 
         </header>
 
-        <div class="banner-container" class="text">
+        <div class="banner-container">
             <h1>
-                <span class="green">CMS pour la </span><span class="red">réalisation du site </span><span class="yellow">d'une mairie</span><br><br>
+                <span class="green">TOWN</span><span class="red"> HALL</span><span class="yellow"> CMS</span><br><br>
             </h1>
         </div>
 
 
         <div class="content">
-            <h2>Description</h2>
-            <p>
-                Ceci est un cms<br>
-                Plus précisément, cette application web (cms) vous permettra de réaliser aisément votre site web présentant votre mairie.<br>
-                Aucune connaissance en programmation web n'est requise, vous n'avez qu'à utiliser ce qui vous est présenté.
-                <br><br>Vous etes actuellement connectés en mode admin.
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nemo, enim neque nobis mollitia pariatur sequi ullam, eius iusto incidunt unde eaque. Esse dolorum fugiat libero accusantium molestiae voluptatum quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odio totam ea nulla vitae nemo pariatur explicabo iusto inventore quaerat asperiores expedita rerum blanditiis officia, sunt nisi similique aliquam. Fugit? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore adipisci consectetur, fuga porro pariatur commodi incidunt id quis soluta nam tempora odio ipsam voluptatem libero? Odio cum vero nulla mollitia. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio quis neque, nesciunt necessitatibus quasi dicta blanditiis dolorem placeat vero, hic laboriosam maiores praesentium possimus sequi debitis provident consequuntur voluptate! Ipsum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa numquam, debitis asperiores quidem, consequatur sunt enim aut magni aliquam nostrum a facilis quae nihil delectus nulla quo error eius nesciunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptate iste corrupti fugit placeat eaque nisi provident, assumenda totam similique possimus eos incidunt quia iusto non sequi et! Fuga, laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque illo quis molestias maxime eligendi unde quas, hic excepturi magni et quaerat odit ab necessitatibus animi quae, reiciendis minima aspernatur nam.<br><br><br>
-            </p>
+            <div class="wrapper">
+                <h2>Description</h2>
+                <p>
+                    Ceci est un cms<br>
+                    Plus précisément, cette application web (cms) vous permettra de réaliser aisément votre site web présentant votre mairie.<br>
+                    Aucune connaissance en programmation web n'est requise, vous n'avez qu'à utiliser ce qui vous est présenté.
+                    <br><br>Vous etes actuellement connectés en mode admin.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nemo, enim neque nobis mollitia pariatur sequi ullam, eius iusto incidunt unde eaque. Esse dolorum fugiat libero accusantium molestiae voluptatum quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odio totam ea nulla vitae nemo pariatur explicabo iusto inventore quaerat asperiores expedita rerum blanditiis officia, sunt nisi similique aliquam. Fugit? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore adipisci consectetur, fuga porro pariatur commodi incidunt id quis soluta nam tempora odio ipsam voluptatem libero? Odio cum vero nulla mollitia. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio quis neque, nesciunt necessitatibus quasi dicta blanditiis dolorem placeat vero, hic laboriosam maiores praesentium possimus sequi debitis provident consequuntur voluptate! Ipsum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa numquam, debitis asperiores quidem, consequatur sunt enim aut magni aliquam nostrum a facilis quae nihil delectus nulla quo error eius nesciunt? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptate iste corrupti fugit placeat eaque nisi provident, assumenda totam similique possimus eos incidunt quia iusto non sequi et! Fuga, laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque illo quis molestias maxime eligendi unde quas, hic excepturi magni et quaerat odit ab necessitatibus animi quae, reiciendis minima aspernatur nam.<br><br><br>
+                </p>
+            </div>
         </div>
     </div>
 
+
+    <footer>
+        <caption>
+
+        </caption>
+    </footer>
     <!--  <div class="footer">
         <br><br><br> Par les étudiants de la faculté des sciences de l'université de Yaoundé I.<br>
         Pour le Travail pratique de l'unité d'enseignement INF2064, programmation web.
